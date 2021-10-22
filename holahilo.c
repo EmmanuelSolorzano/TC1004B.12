f7fb4c0412a81cf787fee7379db6add959b351d0
EmmanuelSolorzano <emahumsol@hotmail.com>
2021-10-22 23:37:11 +0000

    Final

--- holahilo.c
+++ holahilo.c
@@ -2,22 +2,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define NUM_THREADS 5
+#define NUM_THREADS 100
+int saldo;
+
+pthread_mutex_t lockSaldo = PTHREAD_MUTEX_INITIALIZER;
 
 void *printHola(void *arg){
-    printf("Hola desde un hilo\n");
-    while(1){
-        sleep(1);
-    }
+    int saldoLocal;
+    pthread_mutex_lock(&lockSaldo);
+    saldoLocal = saldo;
+    saldoLocal += 100;
+    saldo = saldoLocal;
+    pthread_mutex_unlock(&lockSaldo);
     pthread_exit(NULL);
 }
 
 int main(){
     pthread_t threads[NUM_THREADS];
+    saldo = 0;
     for(int i = 0; i < NUM_THREADS; i++){
         pthread_create(&threads[i],NULL,printHola,NULL);
     }
 
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(threads[i],NULL);
+    }
+
+    printf("El valor del saldo es %d",saldo);
+
     pthread_exit(NULL);
-    return 0;
 }
